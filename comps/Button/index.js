f81c723b0cb55021b98cf51eb8dd4abbaef9adfd
cpark3918 <cindypark31@gmail.com>
2021-02-21 22:05:50 -0800

    contact page done!

--- comps/Button/index.js
+++ comps/Button/index.js
@@ -4,18 +4,30 @@ import styled, {css} from 'styled-components';
 import Router from 'next/router';
 
 const Container = styled.div`
+    min-width: 120px;
+`;
 
+const ButtonBox = styled.button`
+    min-height: 60px;
+    width: 40%;
+    background-color: #272834;
+    color: #fff;
+    border-radius: 2px;
+    border: none;
+    font-family: "GMarketSans";
+    font-size: 18px;
+    text-transform: uppercase;
 `;
 
 
-const Button = ({}) => {
+const Button = ({title}) => {
     return <Container>
-
+        <ButtonBox>{title}</ButtonBox>
     </Container>
 }
 
 Button.defaultProps = {
-
+    title: "submit"
 };
 
 export default Button;
